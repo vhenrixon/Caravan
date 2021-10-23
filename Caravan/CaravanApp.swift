b3be943c1abc254ff0a184f9843fad1535d9d29e
Bruce Tan <bowentan02@gmail.com>
2021-10-23 00:16:05 -0400

    starting

---
+++ Caravan/CaravanApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaravanApp.swift
+//  Caravan
+//
+//  Created by Bowen Tan on 10/22/21.
+//
+
+import SwiftUI
+
+@main
+struct CaravanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Caravan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Caravan
+//
+//  Created by Bowen Tan on 10/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Bruce!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
