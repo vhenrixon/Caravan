5199fdbf8541b922a18e4761083c4e97c239b539
Bruce Tan <bowentan02@gmail.com>
2021-10-23 12:00:05 -0400

    Parsing

--- Caravan/Database.swift
+++ Caravan/Database.swift
@@ -8,17 +8,18 @@
 import Foundation
 import Firebase
 
+
 class Database {
     
     @Published var db: Firestore;
+    @Published var countryList = [ActiveCountries]()
+    var docRef: DocumentReference!
 
+    
     init() {
         FirebaseApp.configure()
         self.db = Firestore.firestore()
     }
-    
-    @Published var tripData = Set<Country>()
-    var docRef: DocumentReference!
     
     func uploadDocument() {
         docRef = db.document("Countries")
@@ -37,10 +38,86 @@ class Database {
             if (error != nil) {
                 print("An Error When Downloading")
             }
-            let myData = docSnapshot.data()
+            let json = try? JSONSerialization.jsonObject(with: docSnapshot.data()!, options: [])
+            guard let dictionary = json as? [String: Any]
+            else {
+                print("Retrieve data error.")
+                return
+            }
+            decodeData(decodeData: dictionary)
         }
     }
     
+    func decodeData(decodeData: Dictionary<String: Any> ) {
+        //decode jason response from FriendListResponse format in FriendFormat.swift
+            do {
+                let decoder = JSONDecoder()
+                let countryData = try decoder.decode(ActiveCountries.self, from: decodeData!)
+                
+                DispatchQueue.main.async {
+                    self.countryList = countryData.countriesCollection
+                    print("Countries are: ", self.countryList)
+                }
+            } catch let error as NSError {
+                print("Error in JSON parsing")
+                print(error.debugDescription)
+            }
+    }
 }
+
+struct Trip: Identifiable, Codable {
+    var date: String;
+    var amountOfPeople: Int;
+    var id: String;
+
+    init(date:String, amountOfPeople:Int, id:String) {
+        self.date = date;
+        self.amountOfPeople = amountOfPeople;
+        self.id = id;
+    }
+
+    func getDate() -> String{
+        return self.date;
+    }
+}
+
+
+
+struct Country: Identifiable, Codable {
+    var id: String;
+    var tripCollection: [Trip];
+
+    init(tripCollection: [Trip], id: String) {
+        self.tripCollection = tripCollection;
+        self.id = id;
+    }
+
+    func getTrip() -> [Trip] {
+        return self.tripCollection;
+    }
+
+
+}
+struct ActiveCountries: Identifiable{
+    var id: String;
+    var countriesCollection: <Country>;
+    init(countriesCollection: <Country>, id: String) {
+        self.countriesCollection = countriesCollection;
+        self.id = id;
+    }
+
+}
+struct User: Identifiable{
+    var id: String;
+    var name: String;
+
+    init(name: String, id: String) {
+        self.name = name;
+        self.id = id;
+    }
+    func getName() -> String {
+        return self.name;
+    }
+ }
 
 
